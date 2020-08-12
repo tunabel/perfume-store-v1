bba73a9e456213ecaca54685c56b96c238af424e
Tuan Ngo <tuan.23a5@gmail.com>
2020-08-12 22:41:05 +0700

    Initial commit

---
+++ src/main/java/com/tunabel/perfumestorev1/PerfumeStoreV1Application.java
@@ -0,0 +1,13 @@
+package com.tunabel.perfumestorev1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfumeStoreV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerfumeStoreV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/tunabel/perfumestorev1/PerfumeStoreV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tunabel.perfumestorev1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfumeStoreV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
